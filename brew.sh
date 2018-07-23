235a2cd401d6a009e2b1b6187477a45e34d15e54
Daniel Bergey <dbergey@barracuda.com>
2018-07-23 15:06:11 -0400

    updates

--- brew.sh
+++ brew.sh
@@ -1,7 +1,11 @@
-brew update
+brew tap homebrew/cask
+brew tap homebrew/cask-fonts
 
 # install homebrew packages
-brew install zsh oh-my-zsh ag git htop nodejs gti tig ssh-copy-id
+brew install zsh ag git htop nodejs gti tig ssh-copy-id
 
 # install essential apps
-brew cask install moom istat-menus alfred vscode iterm2 franz google-chrome resolutionator finicky hazel
+brew cask install moom istat-menus alfred vscode iterm2 franz google-chrome resolutionator finicky hazel safari-technology-preview
+
+# install fonts
+brew cask install font-fira-code font-hack
--- install-brew.sh
+++ install-brew.sh
@@ -1,5 +1,1 @@
-# install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# install app store apps
-# tweetbot3 SafariTechnologyPreview hazel
