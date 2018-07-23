1c01ecde99c35a10fc627db4e2e394da606c5849
Daniel Bergey <dbergey@barracuda.com>
2018-07-23 17:55:03 -0400

    add some app store apps, move things around

--- brew.sh
+++ brew.sh
@@ -13,18 +13,30 @@ brew install ssh-copy-id
 brew install git
 brew install zsh
 
-# install essential apps
-brew cask install moom
+# install free apps
 brew cask install franz
-brew cask install hazel
-brew cask install alfred
 brew cask install iterm2
+brew cask install dropbox
 brew cask install finicky
-brew cask install istat-menus
 brew cask install google-chrome
-brew cask install resolutionator
 brew cask install visual-studio-code
 brew cask install safari-technology-preview
+
+# install paid apps
+brew cask install moom
+brew cask install hazel
+brew cask install alfred
+brew cask install droplr
+brew cask install rocket
+brew cask install 1password6
+brew cask install istat-menus
+brew cask install resolutionator
+
+# install app store apps
+brew install mas
+mas install 766939888 # 1Keyboard
+mas install 1191449274 # ToothFairy
+mas install 1384080005 # Tweetbot 3 for Twitter
 
 # install fonts
 brew cask install font-hack
