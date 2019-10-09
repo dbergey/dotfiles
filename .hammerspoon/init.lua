cd63517d913479700d8b20ee5dab9e30600bebc6
Daniel Bergey <dbergey@gmail.com>
2019-10-09 17:57:45 -0400

    update appkeys

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -18,24 +18,36 @@ hs.hotkey.bind({ 'cmd', 'alt', 'shift'}, 'K', function()
   safari:selectMenuItem('Move Tab to New Window')
 end)
 
--- app hotkeys
-function appKey(modifiers, key, app)
-  hs.hotkey.bind(modifiers, key, function()
-    hs.application.launchOrFocus(app)
+-- app hotkeys (supports fallbacks)
+function appKey(modifiers, key, apps)
+	hs.hotkey.bind(modifiers, key, function()
+		if (type(apps) == 'string') then
+			-- open specified app
+			hs.application.launchOrFocus(apps)
+		else
+			-- launch first app that exists
+			for _, app in ipairs(apps) do
+				if (hs.application.find(app)) then
+					hs.application.launchOrFocus(app)
+					break
+				end
+			end
+		end
   end)
 end
 
-
 appKey({'ctrl', 'alt'}, 'B', 'Visual Studio Code')
 appKey({'ctrl', 'alt'}, 'A', 'Messages')
+appKey({'ctrl', 'alt', 'shift'}, 'A', 'Slack')
 appKey({'ctrl', 'alt'}, 'P', 'System Preferences')
 appKey({'ctrl', 'alt'}, 'D', 'Discord')
 appKey({'ctrl', 'alt'}, 'F', 'Finder')
 appKey({'ctrl', 'alt'}, 'V', 'iTerm')
 appKey({'ctrl', 'alt'}, 'S', 'Safari')
+appKey({'ctrl', 'alt'}, 'J', 'Safari Preview')
 appKey({'ctrl', 'alt', 'shift'}, 'S', 'Google Chrome')
 appKey({'ctrl', 'alt'}, 'H', 'Tweetbot')
-appKey({'ctrl', 'alt'}, 'E', 'Mail')
+appKey({'ctrl', 'alt'}, 'E', { 'Microsoft Outlook', 'Mail' })
 appKey({'ctrl', 'alt'}, 'T', 'iTunes')
 appKey({'ctrl', 'alt'}, 'N', 'Notes')
 
@@ -65,7 +77,7 @@ hs.hotkey.bind({'ctrl', 'alt'}, 'Right', function()
 	local win = hs.window.focusedWindow();
   local cell = hs.grid.getCell(win)
   local toScreen = hs.screen.mainScreen().toEast()
-  
+
 	if not toScreen then
 	  hs.grid.set(win, cell, toScreen)
 	end
