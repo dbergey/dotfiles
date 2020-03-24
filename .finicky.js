926f38acd18bb7405f1215f7045b4dd3bdeed252
Daniel Bergey <dbergey@barracuda.com>
2020-03-24 10:55:00 -0400

    update finicky

--- .finicky.js
+++ .finicky.js
@@ -2,19 +2,23 @@ finicky.setDefaultBrowser('com.apple.Safari');
 
 // Open dev links in chrome
 finicky.onUrl(function(url, opts) {
-    if (url.match(/^https?:\/\/(.*\.docker|.*(\.qa)?\.cudaops\.com|localhost)(\:\d+)?/)) {
-      return {
-        bundleIdentifier: "com.google.Chrome"
-      };
-    }
-  });
-
+	if (
+		url.match(
+			/^https?:\/\/(.*\.docker|.*(\.qa)?\.cudaops\.com|localhost)(\:\d+)?/
+		)
+	) {
+		return {
+			bundleIdentifier: 'com.google.Chrome',
+		};
+	}
+});
 
 // Open JIRA, etc. in safari tech preview
 finicky.onUrl(function(url, opts) {
-    if (url.match(/^https?:\/\/(stash|bugs)\.cudaops\.com/)) {
-      return {
-        bundleIdentifier: "com.apple.SafariTechnologyPreview"
-      };
-    }
-  });
+	if (url.match(/^https?:\/\/(stash|bugs|bccbuild)\.cudaops\.com/)) {
+		return {
+			url: url.replace(/(stash|bugs)\.cudaops\.com/, '$1.cudawork.com'),
+			bundleIdentifier: 'com.apple.SafariTechnologyPreview',
+		};
+	}
+});
